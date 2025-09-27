b58a8594166c5e12cae1d274e1e0045ff27eb2b3
VoinovAnton <32091096+VoinovAnton@users.noreply.github.com>
2025-09-27 11:27:45 +0300

    Update my_dict.py

--- my_dict.py
+++ my_dict.py
@@ -1,11 +1,1 @@
-mydict = {
-    "name": "Anton",
-    "details": {"profession": "god"},
-    "items": ["ball", "computer", "mouse"],
-    "age": 2019
-}
 
-print("My name is " + mydict['name'])
-print("My profession is " + mydict['details']['profession'])
-print("My age is " + str(mydict['age']))
-print("I have " + ",".join(mydict['items']))   # I have ball, computer, mouse
